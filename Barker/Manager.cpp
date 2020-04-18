a7e6eb447985e4ac15c151a85f84b11e74b0187f
metroid2010 <metroid2010@gmail.com>
2020-04-18 14:15:04 +0000

    started class manager, still wip

--- Barker/Manager.cpp
+++ Barker/Manager.cpp
@@ -3,6 +3,59 @@
 * Grupo 9
 * Esther Alonso Pinillos, Roberto Uceda Gomez
 *
-* Implementacion de metodos de clase:
+* Implementacion de metodos de clase: Manager
 *
 **/
+
+#include "Manager.hpp"
+
+Manager::Manager() {
+    _currentUser = 0;
+}
+
+bool Manager::createUser(string email, string password, string username, string bio) {
+
+    // check username does not exist to avoid duplicate users
+    for (uint i = 0; i < _users.size(); i++ ) {
+        if ( _users[i]->getUsername() == username ) {
+            return false;
+        }
+    }
+
+    User* u = new User (email, password, username, bio);
+    _users.push_back(u);
+
+    return true;
+}
+
+vector<PublicUserData*> Manager::showUsers() {
+    // TODO
+}
+
+PublicUserData* Manager::showUser(string username) {
+
+    // check username exists
+    for (uint i = 0; i < _users.size(); i++ ) {
+        if ( _users[i]->getUsername() == username ) {
+            // TODO
+        }
+    }
+
+    // username not found
+    return nullptr;
+}
+
+bool Manager::login(string username, string password) {
+
+    // username exists
+    bool username_exists = false;
+    for (uint i = 0; i < _users.size(); i++ ) {
+        if ( _users[i]->getUsername() == username ) {
+            username_exists = true;
+        }
+    }
+
+    if ( username_exists == true && isLogged(username) )
+
+}
+
--- Barker/Manager.hpp
+++ Barker/Manager.hpp
@@ -3,7 +3,30 @@
 * Grupo 9
 * Esther Alonso Pinillos, Roberto Uceda Gomez
 *
-* Archivo de cabecera:
+* Archivo de cabecera: Manager
 *
 **/
 
+#include <iostream>
+#include <vector>
+#include "User.hpp"
+
+using namespace std;
+
+class Manager {
+
+private:
+    vector<User*> _users;
+    int _currentUser;
+
+public:
+    Manager();
+
+    bool createUser(string email, string password, string username, string bio);
+    vector<PublicUserData*> showUsers();
+    PublicUserData* showUser(string username);
+    bool login(string username, string password);
+    ~Manager();
+
+
+};
