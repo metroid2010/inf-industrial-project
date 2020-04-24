4cf74bf708194189ec8bfd28b3094f6bce12b4fa
metroid2010 <metroid2010@gmail.com>
2020-04-24 13:51:12 +0000

    UML: added searchUser in manager, added missing composition arrows; removed build folders from repo to avoid cluttering

--- build-Barker-Desktop_Qt_5_14_1_GCC_64bit-Debug/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/esther/Escritorio/Informática Industrial/PROYECTO/Proyecto1/build-Barker-Desktop_Qt_5_14_1_GCC_64bit-Debug/googletest-src
-# Build directory: /home/esther/Escritorio/Informática Industrial/PROYECTO/Proyecto1/build-Barker-Desktop_Qt_5_14_1_GCC_64bit-Debug/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build-Barker-Desktop_Qt_5_14_1_GCC_64bit-Debug/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/esther/Escritorio/Informática Industrial/PROYECTO/Proyecto1/build-Barker-Desktop_Qt_5_14_1_GCC_64bit-Debug/googletest-src/googletest
-# Build directory: /home/esther/Escritorio/Informática Industrial/PROYECTO/Proyecto1/build-Barker-Desktop_Qt_5_14_1_GCC_64bit-Debug/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
